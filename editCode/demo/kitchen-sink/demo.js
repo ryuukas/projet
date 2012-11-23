5ecf269f78bb6dc805c44d89467ce58498a35ec9
Rems <debrideurs.infos@gmail.com>
2012-11-24 00:46:40 +0100

    il m'oblige a commit.

--- editCode/demo/kitchen-sink/demo.js
+++ editCode/demo/kitchen-sink/demo.js
@@ -42,7 +42,7 @@ var lang = require("ace/lib/lang");
 var useragent = require("ace/lib/useragent");
 
 var event = require("ace/lib/event");
-var theme = require("ace/theme/textmate");
+var theme = require("ace/theme/cloud");
 var EditSession = require("ace/edit_session").EditSession;
 var UndoManager = require("ace/undomanager").UndoManager;
 
