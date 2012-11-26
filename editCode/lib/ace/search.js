c350438727255e8c253b881825265f300357ed22
macben <b.charpentier@me.com>
2012-11-26 10:53:25 +0100

    few
    fwefefw

--- editCode/lib/ace/search.js
+++ editCode/lib/ace/search.js
@@ -1,37 +1,7 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Distributed under the BSD license:
- *
- * Copyright (c) 2010, Ajax.org B.V.
- * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of Ajax.org B.V. nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL AJAX.ORG B.V. BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * ***** END LICENSE BLOCK ***** */
-
 define(function(require, exports, module) {
 "use strict";
 
-var lang = require("./lib/lang");
+//var lang = require("./lib/lang");
 var oop = require("./lib/oop");
 var Range = require("./range").Range;
 
@@ -82,9 +52,9 @@ var Search = function() {
      * [Returns an object containing all the search options.]{: #Search.getOptions}
      *
     **/
-    this.getOptions = function() {
-        return lang.copyObject(this.$options);
-    };
+//    this.getOptions = function() {
+  //      return lang.copyObject(this.$options);
+  //  };
 
     this.setOptions = function(options) {
         this.$options = options;
@@ -155,11 +125,11 @@ var Search = function() {
             }
         } else {
             for (var i = 0; i < lines.length; i++) {
-                var matches = lang.getMatchOffsets(lines[i], re);
-                for (var j = 0; j < matches.length; j++) {
-                    var match = matches[j];
-                    ranges.push(new Range(i, match.offset, i, match.offset + match.length));
-                }
+//                var matches = lang.getMatchOffsets(lines[i], re);
+  //              for (var j = 0; j < matches.length; j++) {
+    //                var match = matches[j];
+      //              ranges.push(new Range(i, match.offset, i, match.offset + match.length));
+        //        }
             }
         }
 
@@ -256,17 +226,17 @@ var Search = function() {
             };
         } else if (backwards) {
             var matchIterator = function(line, row, startIndex) {
-                var matches = lang.getMatchOffsets(line, re);
-                for (var i = matches.length-1; i >= 0; i--)
-                    if (callback(matches[i], row, startIndex))
-                        return true;
+//                var matches = lang.getMatchOffsets(line, re);
+  //              for (var i = matches.length-1; i >= 0; i--)
+    //                if (callback(matches[i], row, startIndex))
+      //                  return true;
             };
         } else {
             var matchIterator = function(line, row, startIndex) {
-                var matches = lang.getMatchOffsets(line, re);
-                for (var i = 0; i < matches.length; i++)
-                    if (callback(matches[i], row, startIndex))
-                        return true;
+//                var matches = lang.getMatchOffsets(line, re);
+  //              for (var i = 0; i < matches.length; i++)
+    //                if (callback(matches[i], row, startIndex))
+      //                  return true;
             };
         }
 
@@ -287,8 +257,8 @@ var Search = function() {
         if (!options.needle)
             return options.re = false;
 
-        if (!options.regExp)
-            needle = lang.escapeRegExp(needle);
+//        if (!options.regExp)
+  //          needle = lang.escapeRegExp(needle);
 
         if (options.wholeWord)
             needle = "\\b" + needle + "\\b";
