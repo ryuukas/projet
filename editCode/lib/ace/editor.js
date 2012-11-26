13177f689ecd6fff43d6cc4ec6e8d4ce0ec81a75
macben <b.charpentier@me.com>
2012-11-26 11:02:13 +0100

    Revert "fewf"
    This reverts commit 6780ec241df7028c787462374d53035004ac8758.

--- editCode/lib/ace/editor.js
+++ editCode/lib/ace/editor.js
@@ -1,10 +1,40 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Distributed under the BSD license:
+ *
+ * Copyright (c) 2010, Ajax.org B.V.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Ajax.org B.V. nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL AJAX.ORG B.V. BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
 define(function(require, exports, module) {
 "use strict";
 
 require("./lib/fixoldbrowsers");
 
 var oop = require("./lib/oop");
-//var lang = require("./lib/lang");
+var lang = require("./lib/lang");
 var useragent = require("./lib/useragent");
 var TextInput = require("./keyboard/textinput").TextInput;
 var MouseHandler = require("./mouse/mouse_handler").MouseHandler;
@@ -15,7 +45,7 @@ var Search = require("./search").Search;
 var Range = require("./range").Range;
 var EventEmitter = require("./lib/event_emitter").EventEmitter;
 var CommandManager = require("./commands/command_manager").CommandManager;
-//var defaultCommands = require("./commands/default_commands").commands;
+var defaultCommands = require("./commands/default_commands").commands;
 
 /**
  * class Editor
@@ -37,7 +67,7 @@ var Editor = function(renderer, session) {
     this.container = container;
     this.renderer = renderer;
 
-   // this.commands = new CommandManager(useragent.isMac ? "mac" : "win", defaultCommands);
+    this.commands = new CommandManager(useragent.isMac ? "mac" : "win", defaultCommands);
     this.textInput  = new TextInput(renderer.getTextAreaContainer(), this);
     this.renderer.textarea = this.textInput.getElement();
     this.keyBinding = new KeyBinding(this);
@@ -1252,7 +1282,7 @@ var Editor = function(renderer, session) {
                     column      = session.documentToScreenColumn(position.row, position.column),
                     count       = (size - column % size);
 
-//                indentString = lang.stringRepeat(" ", count);
+                indentString = lang.stringRepeat(" ", count);
             } else
                 indentString = "\t";
             return this.insert(indentString);
